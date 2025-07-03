d543d15948c705892a6afc07982c764b40bbb8b0
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-03 14:12:23 +0200

    ottavo punto

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -44,4 +44,9 @@ console.log(teachers[lewisIndex]);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length;
+if (isTeachersEmpty === 0) {
+  console.log(`l'array è vuoto`);
+} else {
+  console.log(`l'array contiene qualche elemento`);
+}
